5f9c90f8fc6575b83b9eb08a3e404a822d589fd3
Geoffrey House <geo.house7@gmail.com>
2022-07-29 16:59:11 -0600

    passing info to each PeacePanel instance

--- src/index.js
+++ src/index.js
@@ -23,7 +23,11 @@ class PeacePanel extends LitElement {
   //     // Will hold the peace language (key) and peace message (value)
   //     messageLanguage:{}
   // };
-
+  static properties = {
+    // This reads in object attached by JS to the HTML element, which contains the
+    // information for the current peace panel.
+    messageDetails: { type: Object },
+  };
   static styles = css`
     .message {
       color: green;
@@ -37,30 +41,30 @@ class PeacePanel extends LitElement {
       width: 300px;
     }
   `;
-  constructor(inputObject) {
-    super();
-    console.log("in constructor");
-    console.log(inputObject);
-    // Assign the input object passed to the class definition when creating a new Object so it's
-    // accessible for correct message/language rendering.
-    this.messageDetails = inputObject;
-    console.log(this);
-  }
+  //   constructor() {
+  //     super();
+  //     console.log("in constructor");
+  //     console.log(this.messageDetails);
+  //     // Assign the input object passed to the class definition when creating a new Object so it's
+  //     // accessible for correct message/language rendering.
+  //     //this.messageDetails = inputObject;
+  //     console.log(this);
+  //   }
 
   render() {
     console.log("in render");
-    console.log(this);
-    return html`<p>Testing</p>`;
-    // return(html`
-    //   <p
-    //     class=${this.messageDetails.displayMessage ? "message" : "language"}
-    //     @click=${() => this.toggleMessage()}
-    //   >
-    //     ${this.messageDetails.displayMessage
-    //       ? this.messageDetails.message
-    //       : this.messageDetails.language}
-    //   </p>
-    // `)
+    console.log(this.messageDetails.language);
+    //return html`<p>Testing</p>`;
+    return html`
+      <p
+        class=${this.messageDetails.displayMessage ? "message" : "language"}
+        @click=${() => this.toggleMessage()}
+      >
+        ${this.messageDetails.displayMessage
+          ? this.messageDetails.message
+          : this.messageDetails.language}
+      </p>
+    `;
   }
 
   // Toggle whether the displayMessage property is set to true/false.
@@ -114,5 +118,11 @@ customElements.define("peace-panel", PeacePanel);
 peaceMessages.forEach((message) => {
   console.log(message);
   const newPeacePanel = document.createElement("peace-panel");
+  // The name of this property added to the HTML node has to exactly match the name expected
+  // in the static properties area of the PeacePanels class definition (this seems to be how it
+  // can process multiple pieces of input information attached to the HTML and correctly assign them.)
+  newPeacePanel.messageDetails = message;
+  console.log("in setting up");
+  console.log(newPeacePanel.messageDetails);
   document.querySelector(".banner-holder").appendChild(newPeacePanel);
 });
